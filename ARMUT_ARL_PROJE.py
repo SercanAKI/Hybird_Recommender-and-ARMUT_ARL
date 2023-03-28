e2b8901ae8f27ed9f8da1bf26af10b286f4db07e
Sef <128620761+SercanAKI@users.noreply.github.com>
2023-03-28 21:25:56 +0300

    Armut ARL Proje

---
+++ ARMUT_ARL_PROJE.py
@@ -0,0 +1,115 @@
+
+#########################
+# İş Problemi
+#########################
+
+# Türkiye’nin en büyük online hizmet platformu olan Armut, hizmet verenler ile hizmet almak isteyenleri buluşturmaktadır.
+# Bilgisayarın veya akıllı telefonunun üzerinden birkaç dokunuşla temizlik, tadilat, nakliyat gibi hizmetlere kolayca
+# ulaşılmasını sağlamaktadır.
+# Hizmet alan kullanıcıları ve bu kullanıcıların almış oldukları servis ve kategorileri içeren veri setini kullanarak
+# Association Rule Learning ile ürün tavsiye sistemi oluşturulmak istenmektedir.
+
+
+#########################
+# Veri Seti
+#########################
+#Veri seti müşterilerin aldıkları servislerden ve bu servislerin kategorilerinden oluşmaktadır.
+# Alınan her hizmetin tarih ve saat bilgisini içermektedir.
+
+# UserId: Müşteri numarası
+# ServiceId: Her kategoriye ait anonimleştirilmiş servislerdir. (Örnek : Temizlik kategorisi altında koltuk yıkama servisi)
+# Bir ServiceId farklı kategoriler altında bulanabilir ve farklı kategoriler altında farklı servisleri ifade eder.
+# (Örnek: CategoryId’si 7 ServiceId’si 4 olan hizmet petek temizliği iken CategoryId’si 2 ServiceId’si 4 olan hizmet mobilya montaj)
+# CategoryId: Anonimleştirilmiş kategorilerdir. (Örnek : Temizlik, nakliyat, tadilat kategorisi)
+# CreateDate: Hizmetin satın alındığı tarih
+
+#########################
+# GÖREV 1: Veriyi Hazırlama
+#########################
+
+# Adım 1: armut_data.csv dosyasınız okutunuz.
+# !pip install mlxtend
+import pandas as pd
+pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 500)
+# çıktının tek bir satırda olmasını sağlar.
+pd.set_option('display.expand_frame_repr', False)
+from mlxtend.frequent_patterns import apriori, association_rules
+
+df = pd.read_csv("5.Hafta/ArmutARL/armut_data.csv")
+df.head()
+df.describe().T
+df.shape
+# Adım 2: ServisID her bir CategoryID özelinde farklı bir hizmeti temsil etmektedir.
+# ServiceID ve CategoryID'yi "_" ile birleştirerek hizmetleri temsil edecek yeni bir değişken oluşturunuz.
+
+df["CombinedId"] = [str(row[1]) + "_" + str(row[2]) for row in df.values]
+df.head()
+
+# Adım 3: Veri seti hizmetlerin alındığı tarih ve saatten oluşmaktadır, herhangi bir sepet tanımı (fatura vb. ) bulunmamaktadır.
+# Association Rule Learning uygulayabilmek için bir sepet (fatura vb.) tanımı oluşturulması gerekmektedir.
+# Burada sepet tanımı her bir müşterinin aylık aldığı hizmetlerdir. Örneğin; 7256 id'li müşteri 2017'in 8.ayında aldığı 9_4, 46_4 hizmetleri bir sepeti;
+# 2017’in 10.ayında aldığı  9_4, 38_4  hizmetleri başka bir sepeti ifade etmektedir. Sepetleri unique bir ID ile tanımlanması gerekmektedir.
+# Bunun için öncelikle sadece yıl ve ay içeren yeni bir date değişkeni oluşturunuz. UserID ve yeni oluşturduğunuz date değişkenini "_"
+# ile birleştirirek ID adında yeni bir değişkene atayınız.
+
+#format=%Y-%m değeri yıl-ay olmak için kullandım.
+#dt.to_period ("M") ile aylık periyotlara böldüm.
+df["New_Date"] = pd.to_datetime(df["CreateDate"], format="%Y-%m").dt.to_period("M")
+df.head()
+
+df["ID"] = [str(row[0]) + "_" + str(row[5]) for row in df.values]
+df.head()
+#########################
+# GÖREV 2: Birliktelik Kuralları Üretiniz
+#########################
+
+# Adım 1: Aşağıdaki gibi sepet hizmet pivot table’i oluşturunuz.
+
+# Hizmet         0_8  10_9  11_11  12_7  13_11  14_7  15_1  16_8  17_5  18_4..
+# SepetID
+# 0_2017-08        0     0      0     0      0     0     0     0     0     0..
+# 0_2017-09        0     0      0     0      0     0     0     0     0     0..
+# 0_2018-01        0     0      0     0      0     0     0     0     0     0..
+# 0_2018-04        0     0      0     0      0     1     0     0     0     0..
+# 10000_2017-08    0     0      0     0      0     0     0     0     0     0..
+
+def create_invoice_service_df(df):
+    return df.groupby(['ID', 'CombinedId'])['CategoryId'].count().unstack().fillna(0). \
+        applymap(lambda x: 1 if x > 0 else 0)
+
+invoice_service_df = create_invoice_service_df(df)
+invoice_service_df.head()
+# Adım 2: Birliktelik kurallarını oluşturunuz.
+frequent_itemsets = apriori(invoice_service_df,
+                            min_support=0.01,
+                            use_colnames=True)
+
+frequent_itemsets.sort_values("support", ascending=False)
+
+rules = association_rules(frequent_itemsets,
+                          metric="support",
+                          min_threshold=0.01)
+
+def create_rules(dataframe):
+    frequent_itemsets = apriori(invoice_service_df, min_support=0.01, use_colnames=True)
+    rules = association_rules(frequent_itemsets, metric="support", min_threshold=0.01)
+
+rules[(rules["support"] > 0.05) & (rules["confidence"] > 0.1) & (rules["lift"] > 2).sort.values("confidence", ascending=False)]
+rules = create_rules(invoice_service_df)
+
+#Adım 3: arl_recommender fonksiyonunu kullanarak en son 2_0 hizmetini alan bir kullanıcıya hizmet önerisinde bulununuz.
+
+def arl_recommender(rules_df, service, rec_count=1):
+    sorted_rules = rules_df.sort_values("lift", ascending=False)
+    recommendation_list = []
+    for i, serv in enumerate(sorted_rules["antecedents"]):
+        for j in list(serv):
+            if j == service:
+                recommendation_list.append(list(sorted_rules.iloc[i]["consequents"])[0])
+
+    return recommendation_list[0:rec_count]
+
+arl_recommender(rules, "2_0", 5)
+
